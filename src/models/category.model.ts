218c8d68a4846a6f14cc212710d38fd07cee6969
JeganPC <49855459+jeganswiz@users.noreply.github.com>
2024-10-07 01:43:46 +0530

    sicked in commit check

--- src/models/category.model.ts
+++ src/models/category.model.ts
@@ -3,6 +3,7 @@ import { Document, model, Schema } from "mongoose";
 export interface IVoicememo extends Document {
   notes: string;
   image: string;
+  userId: string;
   isBlocked: boolean;
   createdAt: Date;
   updatedAt: Date;
@@ -12,6 +13,7 @@ const VoicememoSchema: Schema = new Schema(
   {
     notes: { type: String, required: true },
     file: { type: String, required: true },
+    userId: { type: String, required: true },
     isBlocked: { type: Boolean, default: false },
   },
   { collection: "voicememos", timestamps: true }
